263589b06cdb4686bebfe6c09c7c67c0f726c171
Shyla27 <sheilabernicew@gmail.com>
2023-07-06 18:05:49 +0300

    new packages

--- assets/js/main.js
+++ assets/js/main.js
@@ -268,39 +268,8 @@
 
 })()
 
+$('#myModal').modal(options)
 
-const firebaseConfig = {
-  //   copy your firebase config informations
-};
-
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
-
-// reference your database
-var contactFormDB = firebase.database().ref("contactForm");
-
-document.getElementById("contactForm").addEventListener("submit", submitForm);
-
-function submitForm(e) {
-  e.preventDefault();
-
-  var name = getElementVal("name");
-  var emailid = getElementVal("emailid");
-  var msgContent = getElementVal("msgContent");
-
-  saveMessages(name, emailid, msgContent);
-
-  //   enable alert
-  document.querySelector(".alert").style.display = "block";
-
-  //   remove the alert
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
-
-  //   reset the form
-  document.getElementById("contactForm").reset();
-}
 
 const saveMessages = (name, emailid, msgContent) => {
   var newContactForm = contactFormDB.push();
